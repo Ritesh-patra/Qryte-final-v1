934020a519525e90ceee76659b86237e2bb4b492
Ritesh-patra <patrasagarika654@gmail.com>
2025-12-16 13:18:03 +0530

    add soming new

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { OrderProvider } from './context/OrderContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/QRYTE_Final/">
+    <BrowserRouter basename="/">
       <OrderProvider>
         <App />
       </OrderProvider>
